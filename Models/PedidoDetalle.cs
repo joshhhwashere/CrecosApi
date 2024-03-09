07cf38f6ab3d831e64c42848c5ccc7438a42e5e7
joshhhwashere <137361031+joshhhwashere@users.noreply.github.com>
2024-03-09 12:01:35 -0500

    PedidoDetalle.cs

--- Models/PedidoDetalle.cs
+++ Models/PedidoDetalle.cs
@@ -8,11 +8,17 @@ namespace CrecosApi.Models
         [Key]
         public int Id { get; set; }
         public int PedidoId { get; set; }
-        public int ProductoId { get; set; }
-        public int Cantidad { get; set; }
-        public decimal Costo { get; set; }
-
         public Pedido Pedido { get; set; }
+        public int ProductoId { get; set; }
         public Producto Producto { get; set; }
+        public string Nombre { get; set; }
+
+        // Constructor
+        public PedidoDetalle()
+        {
+            Pedido = new Pedido();
+            Producto = new Producto();
+            Nombre = "NombrePredeterminado";
+        }
     }
 }
