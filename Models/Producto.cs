d425728b8372a54a648452fb53b8ed3d10ee087a
joshhhwashere <137361031+joshhhwashere@users.noreply.github.com>
2024-03-09 12:02:31 -0500

    Producto.cs

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -4,12 +4,16 @@ namespace CrecosApi.Models
 {
     public class Producto
     {
-        [Key]
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public decimal Costo { get; set; }
-        public bool Estado { get; set; }
-        public List<PedidoDetalle> DetallesDePedido { get; set; }
+            [Key]
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public List<PedidoDetalle> DetallesDePedido { get; set; }
 
+            // Constructor
+            public Producto()
+            {
+                Nombre = "NombrePredeterminado";
+                DetallesDePedido = new List<PedidoDetalle>();  // Asignar una lista vacía por defecto
+            }
     }
 }
